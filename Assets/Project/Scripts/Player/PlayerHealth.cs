2108c6fcaabb5af0b2edc85cefa40efc4effa6be
oppa073-hub <oppa073@gmail.com>
2026-07-01 22:12:42 +0900

    <feat> 플레이어HP UI 구현

--- Assets/Project/Scripts/Player/PlayerHealth.cs
+++ Assets/Project/Scripts/Player/PlayerHealth.cs
@@ -1,19 +1,26 @@
 ﻿using UnityEngine;
-
+using System;
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] private int maxHp = 10;
 
     private int currentHp;
+    public int CurrentHp => currentHp;
+    public int MaxHp => maxHp;
+
+    public event Action<int, int> OnHpChanged;
 
     private void Awake()
     {
         currentHp = maxHp;
+        OnHpChanged?.Invoke(currentHp, maxHp);
     }
 
     public void TakeDamage(int damage)
     {
         currentHp -= damage;
+
+        OnHpChanged?.Invoke(currentHp, maxHp);
 
         if (currentHp <= 0)
         {
