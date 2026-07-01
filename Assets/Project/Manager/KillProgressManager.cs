5b30d895c92895488bac1d8d648d03bf5f067f21
oppa073-hub <oppa073@gmail.com>
2026-07-01 22:40:02 +0900

    <feat.> 웨이브 바 UI 구현

--- Assets/Project/Manager/KillProgressManager.cs
+++ Assets/Project/Manager/KillProgressManager.cs
@@ -1,19 +1,28 @@
 ﻿using UnityEngine;
-
+using UnityEngine.UI;
+using TMPro;
 public class KillProgressManager : MonoBehaviour
 {
     [SerializeField] private int[] killMilestones;
     [SerializeField] private SkillSelectionManager skillSelectionManager;
     [SerializeField] private MonsterSpawnManager monsterSpawnManager;
+    [SerializeField] private Image progressSlider;
+    [SerializeField] private TMP_Text WaveText;
     private int currentKillCount;
     private int milestoneIndex;
     private bool isFinalConditionReached;
+
+    private void Start()
+    {
+        UpdateWaveUI();
+    }
 
     public void HandleMonsterDead(MonsterHealth monster)
     {
         monster.OnDead -= HandleMonsterDead;
 
         currentKillCount++;
+        UpdateWaveUI();
 
         if (milestoneIndex >= killMilestones.Length)
             return;
@@ -21,6 +30,7 @@ public class KillProgressManager : MonoBehaviour
         if (currentKillCount >= killMilestones[milestoneIndex])
         {
             milestoneIndex++;
+            UpdateWaveUI();
 
             if (milestoneIndex >= killMilestones.Length)
             {
@@ -41,5 +51,29 @@ public class KillProgressManager : MonoBehaviour
         {
             GameManager.Instance.ChangeState(GameState.GameClear);
         }
+    }
+    private void UpdateWaveUI()  //웨이브 UI 업데이트
+    {
+        if (milestoneIndex >= killMilestones.Length)
+        {
+            progressSlider.fillAmount = 1f;
+            WaveText.text = killMilestones.Length.ToString();
+            return;
+        }
+
+        int previousTarget = 0;
+
+        if (milestoneIndex > 0)
+        {
+            previousTarget = killMilestones[milestoneIndex - 1];
+        }
+
+        int currentTarget = killMilestones[milestoneIndex];
+
+        int currentWaveKill = currentKillCount - previousTarget;
+        int requiredWaveKill = currentTarget - previousTarget;
+
+        progressSlider.fillAmount = (float)currentWaveKill / requiredWaveKill;
+        WaveText.text = (milestoneIndex + 1).ToString();
     }
 }
