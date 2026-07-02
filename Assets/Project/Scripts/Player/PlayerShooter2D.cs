c79a61b6ba0340f7ac6b93443f93660e3e27e4e1
oppa073-hub <oppa073@gmail.com>
2026-07-02 22:34:17 +0900

    <Refactor> 모바일 환경 조작

--- Assets/Project/Scripts/Player/PlayerShooter2D.cs
+++ Assets/Project/Scripts/Player/PlayerShooter2D.cs
@@ -15,13 +15,41 @@ public class PlayerShooter2D : MonoBehaviour
     }
     private void Update()
     {
+        Vector2 inputPosition;
+        bool isPressed = false;
+
+#if UNITY_EDITOR
         if (Input.GetMouseButton(0))
         {
-            Vector2 mouseWorldPos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
-            Vector2 dir = mouseWorldPos - (Vector2)transform.position;
-
-            if (dir.y > 0f) aimDirection = dir.normalized;
+            inputPosition = Input.mousePosition;
+            isPressed = true;
         }
+        else
+        {
+            inputPosition = Vector2.zero;
+        }
+#else
+    if (Input.touchCount > 0)
+    {
+        Touch touch = Input.GetTouch(0);
+        inputPosition = touch.position;
+        isPressed = true;
+    }
+    else
+    {
+        inputPosition = Vector2.zero;
+    }
+#endif
+
+        if (isPressed)
+        {
+            Vector2 worldPos = mainCamera.ScreenToWorldPoint(inputPosition);
+            Vector2 dir = worldPos - (Vector2)transform.position;
+
+            if (dir.y > 0f)
+                aimDirection = dir.normalized;
+        }
+
         trajectoryPreview.DrawTrajectory(transform.position, aimDirection);
     }
 }
